b78ae604646c8abf42c6903110fc1a913b63bd49
keireasterbrook <keireasterbrook@gmail.com>
2024-03-12 09:08:44 +0000

    adds comment and loading functionality to the individual articles

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './Article.css';
 import newsApi from '../utils/newsAPI'; 
+import Loading from '../Loading/Loading';
+import Comments from '../Comments/Comments';
+
 
 function formatDate(createdAtDate) {
     const date = new Date(createdAtDate);
@@ -12,17 +15,23 @@ const Article = () => {
     const params = useParams();
     const articleId = params.article_id
     const [article, setArticle] = useState([]);
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         newsApi.get(`/articles/${articleId}`)
         .then(response => {
             const data = response.data.article;
-            setArticle(data)  
+            setArticle(data)
+            setIsLoading(false)
         })
     }, [articleId]);
 
+    if(isLoading){
+        return <Loading />
+    }
 
     return (
+        <>
         <div className='article'>
             <h1 className='articleTitle'>{article.title}</h1>
             <img src={article.article_img_url} alt="article image" className='articleImg' />
@@ -32,6 +41,11 @@ const Article = () => {
             <p className='articleVotes'>Votes: {article.votes}</p>
             <p className='articleTime'>Date Posted: {formatDate(article.created_at)}</p>
         </div>
+        <div>
+            <Comments articleId={articleId}/>
+        </div>
+        </>
+        
     );
 }
 
